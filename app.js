dc305e378865e4163cb94b2d20f3d5813d47915d
Redwan islam <redwanislam2760@gmail.com>
2023-07-25 22:39:24 +0600

    complete problem 1 reverse string

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // Problem 1
-//  Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method. 
+//  Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+function reverse(str) {
+  let reversedStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+
+  return reversedStr;
+}
+
+reverse("hello world");
+
 
